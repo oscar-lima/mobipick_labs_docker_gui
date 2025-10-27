6866fedd8eecef7cd51c086c826de53ec9144b61
Oscar Lima <olima_84@yahoo.com>
2025-10-27 19:26:48 +0100

    Ensure custom entrypoint mount resolves to absolute path

--- mobipick_gui/config.py
+++ mobipick_gui/config.py
@@ -96,6 +96,7 @@ CONFIG_DEFAULTS: Dict[str, Dict] = {
             'COMPOSE_IGNORE_ORPHANS': '1',
             'COMPOSE_FILE': str(DOCKER_COMPOSE_FILE),
             'COMPOSE_PROJECT_NAME': 'mobipick',
+            'MOBIPICK_ENTRYPOINT_HOST_PATH': str(PROJECT_ROOT / 'custom_entrypoint.sh'),
         },
         'compose_run_env': {
             'PYTHONUNBUFFERED': '1',
