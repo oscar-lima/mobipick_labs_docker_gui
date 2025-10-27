b8b25d0430c888137b876cd18f86eb2de2186920
Oscar Lima <olima_84@yahoo.com>
2025-10-27 19:11:44 +0100

    Add verbose logging to custom entrypoint script

--- mobipick_gui/resources/custom_entrypoint.sh
+++ mobipick_gui/resources/custom_entrypoint.sh
@@ -1,7 +1,32 @@
 #!/bin/bash
-set -e
+set -Eeuo pipefail
 
-# setup gazebo and ros environment
-source "/usr/share/gazebo/setup.sh" --
-source "/root/catkin_ws/devel/setup.bash" --
+log() {
+  echo "[entrypoint] $*" >&2
+}
+
+trap 'log "error while running: ${BASH_COMMAND} (line ${LINENO})"' ERR
+
+log "starting entrypoint"
+log "current user: $(id -u):$(id -g)"
+log "working directory: $(pwd)"
+log "command: $*"
+
+if [[ -f /usr/share/gazebo/setup.sh ]]; then
+  log "sourcing /usr/share/gazebo/setup.sh"
+  # setup gazebo environment
+  source "/usr/share/gazebo/setup.sh" --
+else
+  log "warning: /usr/share/gazebo/setup.sh not found"
+fi
+
+if [[ -f /root/catkin_ws/devel/setup.bash ]]; then
+  log "sourcing /root/catkin_ws/devel/setup.bash"
+  # setup ros environment
+  source "/root/catkin_ws/devel/setup.bash" --
+else
+  log "warning: /root/catkin_ws/devel/setup.bash not found"
+fi
+
+log "executing: $*"
 exec "$@"
